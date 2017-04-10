06db22fb7ba22c7f772b62b7dcf01316b8d450b4
HouJP <houjp1992@gmail.com>
2017-04-10 21:59:13 +0800

    add BTM.save_all_question2wids

--- houjp/bin/btm.py
+++ houjp/bin/btm.py
@@ -5,9 +5,55 @@ import ConfigParser
 import pandas as pd
 from utils import DataUtil
 import nltk
+from utils import LogUtil
 
 
 class BTM(object):
+
+    @staticmethod
+    def load_w2id(w2id_fp):
+        """
+        加载Map(word, word_id)
+        :param w2id_fp:
+        :return:
+        """
+        w2id = {}
+        f = open(w2id_fp, 'r')
+        for line in f:
+            [wid, w] = line.strip().split()
+            w2id[w] = int(wid)
+        f.close()
+        LogUtil.log('INFO', 'load w2id done (%s)' % w2id_fp)
+        return w2id
+
+    @staticmethod
+    def save_all_question2wids():
+        """
+        将train.csv、test.csv语句转化为word_id列表
+        :return:
+        """
+        LogUtil.log('INFO', 'BEGIN: save all question2wids')
+        # 读取配置文件
+        cf = ConfigParser.ConfigParser()
+        cf.read("../conf/python.conf")
+
+        # 获取文件路径
+        qid2question_question_fp = '%s/qid2question.all.question' % cf.get('DEFAULT', 'devel_pt')
+        w2id_fp = '/home/houjianpeng/BTM/output/train_100_50/voca.txt'
+        all_question_wids_fp = '/home/houjianpeng/BTM/output/train_100_50/all_doc_wids.txt'
+
+        # 加载词典
+        w2id = BTM.load_w2id(w2id_fp)
+
+        all_question_f = open(qid2question_question_fp, 'r')
+        all_question_wids_f = open(all_question_wids_fp, 'w')
+        for line in all_question_f:
+            ws = line.strip().split()
+            wids = [w2id[w] for w in ws if w in w2id]
+            print >> all_question_wids_f, ' '.join(map(str, wids))
+        all_question_f.close()
+        all_question_wids_f.close()
+        LogUtil.log('INFO', 'END: save all question2wids')
 
     @staticmethod
     def get_qid2question(df):
@@ -94,6 +140,7 @@ class BTM(object):
 if __name__ == "__main__":
     # 存储qid和question文件
     # BTM.save_all_qid2question()
-    BTM.save_train_qid2question()
+    # BTM.save_train_qid2question()
+    BTM.save_all_question2wids()
 
 
