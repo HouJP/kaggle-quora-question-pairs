eff391c634b3c0f7f1ca9166ed7110806ad1d202
HouJP <houjp1992@gmail.com>
2017-05-03 15:07:49 +0800

    add extractor.Graph.extract_graph_num_clique

--- houjp/bin/extractor.py
+++ houjp/bin/extractor.py
@@ -2507,7 +2507,7 @@ class Graph(object):
     @staticmethod
     def extract_graph_edge_max_clique_size(cf, argv):
         # 设置参数
-        feature_name = 'graph_edge_clique_num'
+        feature_name = 'graph_edge_max_clique_size'
 
         Graph.init_graph(cf, argv)
 
@@ -2541,11 +2541,79 @@ class Graph(object):
         Feature.save_dataframe(test_features, test_feature_fp)
         LogUtil.log('INFO', 'save test features (%s) done' % feature_name)
 
+    @staticmethod
+    def extract_row_graph_num_clique(row, *args):
+        n2clique = args[0]
+        cliques = args[1]
+
+        q1 = str(row['question1']).strip()
+        q2 = str(row['question2']).strip()
+
+        qid1 = Graph.q2id[q1]
+        qid2 = Graph.q2id[q2]
+
+        num_clique = 0
+
+        for clique_id in n2clique[qid1]:
+            if qid2 in cliques[clique_id]:
+                num_clique += 1
+
+        return [num_clique]
+
+    @staticmethod
+    def extract_graph_num_clique(cf, argv):
+        """
+        计算包含边的团的数量
+        :param cf:
+        :param argv:
+        :return:
+        """
+        # 设置参数
+        feature_name = 'num_clique'
+
+        Graph.init_graph(cf, argv)
+
+        n2clique = {}  # 节点到团的映射
+        cliques = []  # 所有团
+        for clique in nx.find_cliques(Graph.G):
+            for n in clique:
+                if n not in n2clique:
+                    n2clique[n] = []
+                n2clique[n].append(len(cliques))
+            cliques.append(clique)
+        LogUtil.log('INFO', 'len(cliques)=%d' % len(cliques))
+
+        # 加载数据文件
+        train_data = pd.read_csv('%s/train.csv' % cf.get('DEFAULT', 'origin_pt')).fillna(value="")
+        test_data = pd.read_csv('%s/test.csv' % cf.get('DEFAULT', 'origin_pt')).fillna(value="")
+
+        # 特征存储路径
+        feature_pt = cf.get('DEFAULT', 'feature_question_pair_pt')
+        train_feature_fp = '%s/%s.train.smat' % (feature_pt, feature_name)
+        test_feature_fp = '%s/%s.test.smat' % (feature_pt, feature_name)
+
+        # 抽取特征：train.csv
+        train_features = train_data.apply(Graph.extract_row_graph_num_clique, axis=1, raw=True,
+                                          args=[n2clique, cliques])
+        LogUtil.log('INFO', 'extract train features (%s) done' % feature_name)
+        Feature.save_dataframe(train_features, train_feature_fp)
+        LogUtil.log('INFO', 'save train features (%s) done' % feature_name)
+
+        test_features = test_data.apply(Graph.extract_row_graph_num_clique, axis=1, raw=True,
+                                        args=[n2clique, cliques])
+        LogUtil.log('INFO', 'extract test features (%s) done' % feature_name)
+        Feature.save_dataframe(test_features, test_feature_fp)
+        LogUtil.log('INFO', 'save test features (%s) done' % feature_name)
 
 
     @staticmethod
     def run(cf, argv):
-        Graph.extract_graph_edge_max_clique_size(cf, argv)
+        cmd = argv[0]
+
+        if 'extract_graph_edge_max_clique_size' == cmd:
+            Graph.extract_graph_edge_max_clique_size(cf, argv[1:])
+        elif 'extract_graph_num_clique' == cmd:
+            Graph.extract_graph_num_clique(cf, argv[1:])
 
 def print_help():
     print 'extractor <conf_file_fp> -->'
