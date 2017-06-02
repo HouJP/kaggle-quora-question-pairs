1204ab4c67ec3cb6eed8397f9604a6345f702f6a
HouJP <houjp1992@gmail.com>
2017-06-02 13:57:53 +0800

    update FeatureProcessor.run_gen_feature_with_extra

--- houjp/bin/featureprocessor.py
+++ houjp/bin/featureprocessor.py
@@ -190,7 +190,7 @@ class FeatureProcessor(object):
         if 'run_gen_feature_with_swap' == cmd:
             FeatureProcessor.run_gen_feature_swap(cf, argv[1:])
             FeatureProcessor.run_gen_feature_with_swap(cf, argv[1:])
-        elif 'run_gen_feature_extra' == cmd:
+        elif 'run_gen_feature_with_extra' == cmd:
             FeatureProcessor.run_gen_feature_extra(cf)
             FeatureProcessor.run_gen_feature_with_extra(cf, argv[1:])
         else:
