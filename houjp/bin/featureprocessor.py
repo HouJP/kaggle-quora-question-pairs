faccc2d724690d12d9d7d675cc2269189db7e804
HouJP <houjp1992@gmail.com>
2017-06-02 13:19:25 +0800

    update FeatureProcessor.run_gen_feature_with_swap

--- houjp/bin/featureprocessor.py
+++ houjp/bin/featureprocessor.py
@@ -61,7 +61,7 @@ class FeatureProcessor(object):
         return f_names, f_indexs
 
     @staticmethod
-    def run_gen_feature_swap(cf):
+    def run_gen_feature_swap(cf, argv):
         """
         交换<Q1,Q2>特征
         :return:
@@ -69,6 +69,7 @@ class FeatureProcessor(object):
         # 读取配置文件
         # cf = ConfigParser.ConfigParser()
         # cf.read(conf_fp)
+        rawset_name = argv[0]
         feature_pt = cf.get('DEFAULT', 'feature_question_pair_pt')
 
         # 加载配置文件
@@ -80,11 +81,10 @@ class FeatureProcessor(object):
             f_name = f_names[i]
             f_index = f_indexs[i]
 
-            rawset_name = 'train'
             FeatureProcessor.swap_feature(feature_pt, f_name, f_index, rawset_name)
 
     @staticmethod
-    def run_gen_feature_with_swap(cf):
+    def run_gen_feature_with_swap(cf, argv):
         """
         生成线下特征文件，包含swap部分
         :return:
@@ -95,7 +95,7 @@ class FeatureProcessor(object):
         feature_pt = cf.get('DEFAULT', 'feature_question_pair_pt')
 
         feature_qp_names = Feature.get_feature_names_question_pair(cf)
-        rawset_name = 'train'
+        rawset_name = argv[0]
 
         for f_name in feature_qp_names:
             feature_fp = '%s/%s.%s.smat' % (feature_pt, f_name, rawset_name)
@@ -161,8 +161,8 @@ class FeatureProcessor(object):
     def run(cf, argv):
         cmd = argv[0]
         if 'run_gen_feature_with_swap' == cmd:
-            FeatureProcessor.run_gen_feature_swap(cf)
-            FeatureProcessor.run_gen_feature_with_swap(cf)
+            FeatureProcessor.run_gen_feature_swap(cf, argv[1:])
+            FeatureProcessor.run_gen_feature_with_swap(cf, argv[1:])
         elif 'run_gen_feature_extra' == cmd:
             FeatureProcessor.run_gen_feature_extra(cf)
         else:
